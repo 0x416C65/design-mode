681de636302474278bed76ba20f133a53f7c603f
└ Disco Local (C:) <0x416c+github@gmail.com>
2023-10-09 22:31:21 -0300

    Update index.ts

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,7 +1,5 @@
 import { setupContextMenu } from './context-menu';
 import { setupAction } from './action';
 
-chrome.runtime.onInstalled.addListener(() => {
-  setupContextMenu();
-  setupAction();
-});
+setupContextMenu();
+setupAction();
